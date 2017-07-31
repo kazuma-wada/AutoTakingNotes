1152f02041503a5e69565049c44fd8554d2744cc
Kazuma Wada <kazuma.wada@isana.net>
2017-07-31 12:14:40 +0900

    ダブルタップのリスナーを追加

--- app/src/main/java/com/example/internship/autotakingnotes/CreateNotesActivity.java
+++ app/src/main/java/com/example/internship/autotakingnotes/CreateNotesActivity.java
@@ -1,13 +1,46 @@
 package com.example.internship.autotakingnotes;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
 
 public class CreateNotesActivity extends AppCompatActivity {
+
+    private static final String TAG = "CreateNotesActivity:";
+
+    private ConstraintLayout constraintLayout;
+    private GestureDetector gestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_notes);
+
+        constraintLayout = (ConstraintLayout) findViewById(R.id.constraintLayout);
+        gestureDetector = new GestureDetector(this, onGestureListener);
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        gestureDetector.onTouchEvent(event);
+        return false;
+    }
+
+    private final GestureDetector.SimpleOnGestureListener onGestureListener = new GestureDetector.SimpleOnGestureListener() {
+
+        /**
+         * ダブルタップされたときの処理
+         * @param e motion event
+         * @return
+         */
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
+            return super.onDoubleTap(e);
+        }
+    };
+
 }
