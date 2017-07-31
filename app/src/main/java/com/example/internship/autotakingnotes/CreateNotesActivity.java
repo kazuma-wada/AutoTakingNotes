0916ee36fda8b5a14c29db77472a0cce7bc2236b
intern <intern@ISN57-no-iMac.local>
2017-07-31 18:19:46 +0900

    終了ボタンの追加 ダイアログの表示の追加

--- app/src/main/java/com/example/internship/autotakingnotes/CreateNotesActivity.java
+++ app/src/main/java/com/example/internship/autotakingnotes/CreateNotesActivity.java
@@ -1,12 +1,19 @@
 package com.example.internship.autotakingnotes;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 public class CreateNotesActivity extends AppCompatActivity {
 
@@ -23,6 +30,29 @@ public class CreateNotesActivity extends AppCompatActivity {
         constraintLayout = (ConstraintLayout) findViewById(R.id.constraintLayout);
         gestureDetector = new GestureDetector(this, onGestureListener);
     }
+
+    public void endCreate(View viwe) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("ノート作成を");
+        alert.setMessage("ノート作成を終了しますか？");
+        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener(){
+          public void onClick(DialogInterface dialog , int which){
+              //ここにYESの処理
+              Intent intent = new Intent(CreateNotesActivity.this,MainActivity.class);
+              startActivity(intent);
+              Log.d(TAG, "ダイアログ:YES");
+          }});
+
+        alert.setNegativeButton("No", new DialogInterface.OnClickListener(){
+            public void onClick(DialogInterface dialog , int which){
+                //ここにNOの処理
+                Log.d(TAG,":NO");
+                //Toast.makeText(CreateNotesActivity.this, "Yes!!" , Toast.LENGTH_LONG).show();
+            }});
+
+        alert.show();
+    }
+
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
@@ -42,5 +72,4 @@ public class CreateNotesActivity extends AppCompatActivity {
             return super.onDoubleTap(e);
         }
     };
-
 }
